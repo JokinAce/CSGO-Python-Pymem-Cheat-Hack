23e1d5d8082e6e53e3761148c414f1db4e0e1715
JokinAce <pisferko@gmail.com>
2020-07-28 14:02:23 +0000

    :bug: Fixing to public repository

--- CSGO Cheat/offsets.py
+++ CSGO Cheat/offsets.py
@@ -1,6 +1,6 @@
 import requests, re, math
 
-r = requests.get("https://raw.githubusercontent.com/JokinAce/CSGO-Python-Pymem-Cheat-Hack/master/csgo.hpp?token=ANWTVITBFMELXF2IGF24LJC7EAU66") #Link to mine
+r = requests.get("https://raw.githubusercontent.com/JokinAce/CSGO-Offsets/master/csgo.hpp")
 r = r.text
 
 
