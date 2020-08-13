36a1faa48114eae6633752d0285a4f8a086bd534
Dom™ <61916282+Dominiggas@users.noreply.github.com>
2020-08-13 20:40:15 +0200

    Update main.py

--- CSGO Cheat/main.py
+++ CSGO Cheat/main.py
@@ -186,7 +186,7 @@ def Main():
         if keyboard.is_pressed("space"):
             force_jump = client + dwForceJump
             on_ground = pm.read_int(player + m_fFlags)
-            if player and on_ground and on_ground == 257:
+            if player and on_ground and on_ground == 257 or on_ground == 263:
                 pm.write_int(force_jump, 5)
                 time.sleep(0.08)
                 pm.write_int(force_jump, 4)
